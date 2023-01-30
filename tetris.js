53aaf62f3846656fa79cb84923069669d7a93a7d
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-01-30 01:06:11 -0500

    Add scoreboard

--- tetris.js
+++ tetris.js
@@ -4,9 +4,13 @@ let gBArrayHeight = 20;
 let gBArrayWidth = 12;
 let startX = 4;
 let startY = 0;
+let score = 0;
+let level = 1;
+let winOrLose = "Playing";
 let blockDimension = 21;
 //initialize 2D array with format [[{x:111, y:222}], [{x:, y:}], [{x:, y:}]...]...
 let coordinateArray = [...Array(gBArrayHeight)].map(() => Array(gBArrayWidth).fill(0));
+let stoppedShapeArray = [...Array(gBArrayHeight)].map(() => Array(gBArrayWidth).fill(0));
 
 /*  T block: [[1,0],[0,1],[1,1],[2,1]]
     0   1   2   3
@@ -68,6 +72,29 @@ function SetupCanvas() {
 
     ctx.strokeStyle = 'black';
     ctx.strokeRect(8, 8, 280, 462);
+
+    ctx.fillStyle = 'black';
+    ctx.font = '21ps Arial';
+    ctx.fillText("SCORE", 300, 98);
+
+    ctx.strokeRect(300, 107, 161, 24)
+    ctx.fillText(score.toString(), 310, 127);
+
+    ctx.fillText("LEVEL", 300, 157);
+    ctx.strokeRect(300, 171, 161, 24);
+    ctx.fillText(level.toString(), 310, 190);
+
+    ctx.fillText("WIN / LOSE", 300, 221);
+    ctx.fillText(winOrLose, 310, 261);
+    ctx.strokeRect(300, 232, 161, 95);
+
+    ctx.fillText("CONTROLS", 300, 354);
+    ctx.strokeRect(300, 366, 161, 104);
+    ctx.font = '19px Arial';
+    ctx.fillText("A : Move Left", 310, 388);
+    ctx.fillText("D : Move Right", 310, 413);
+    ctx.fillText("S : Move Down", 310, 438);
+    ctx.fillText("E : Rotate Right", 310, 463);
 
     document.addEventListener('keydown', HandleKeyPress);
     CreateTetrominos();
