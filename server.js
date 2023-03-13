0392de846c599c4de9bdae861ac000218d6e5b92
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-03-12 23:05:55 -0400

    Add db connection & mongoose Schema

--- server.js
+++ server.js
@@ -1,26 +1,72 @@
+require('dotenv').config();
 const app = require('express')();
-const PORT = 8080;
+const mongoose = require('mongoose');
+let PORT = process.env.PORT;
+let uri = process.env.MONGO_URI;
 
-app.listen(
-    PORT,
-    () => console.log(`server running on port ${PORT}`)
-);
+//var MongoClient = require('mongodb').MongoClient;
+// app.listen(
+//     PORT,
+//     () => console.log(`server listening on port ${PORT}`)
+// );
 
-app.get('/highscores', (req, res)=>{
-    res.status(200).send({
-        name: 'mename',
-        score: 1000,
-    });
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB")
+        app.listen(
+            PORT,
+            () => console.log(`server listening on port ${PORT}`)
+        );
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+connect();
+
+const Schema = mongoose.Schema;
+const scoreSchema = new Schema({
+    name: { type: String, required: true },
+    score: { type: Number, required: true },
+    lines: { type: Number, required: true },
+    level: { type: Number, required: true },
+}, { timestamps: true });
+const Highscore = mongoose.model('Highscore', scoreSchema);
+
+app.get('/', (req, res) => {
+    //res.render('tetris');
 })
 
-//GET
+app.get('/highscores', (req, res) => {
+    Highscore.find()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+})
+
+app.get('/add-score', (req, res) => {
+    const scoreDoc = new Highscore({ name: "testris1", score: 5000, lines: 6, level: 1 });
+    scoreDoc.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+})
+
+//GET from route
 //connect to db
 //if high score button clicked, return top 10 scores in order
 
 //POST
 //on gameover
 //compare score to #10 highscore
-//if greater, prompt user for name
+//if greater, prompt user for name, then POST to route
 //on submit, insert name into DB
 //then display all high scores 
 //(and delete old record?)
