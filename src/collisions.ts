181b0d0656aff85b87646c6b999a92792bd68686
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-03 02:58:41 -0400

    Export collision functions to separate file

---
+++ src/collisions.ts
@@ -0,0 +1,114 @@
+import { gBArrayHeight, gBArrayWidth, stoppedShapeArray, startX, startY, curTetromino, curTetrominoImage, rotationIndex, 
+    mod,
+    checkForCompletedRows,
+    createTetrominoFromNext,
+    drawCurTetrominoAndCheckGameOver, } from './tetris'
+
+//-------------\\
+//  COLLISIONS  \\
+//---------------\\
+function floorCollision(y: number): boolean {
+    return y >= gBArrayHeight;
+}
+
+function wallCollision(x: number): boolean {
+    return x > gBArrayWidth - 1 || x < 0;
+}
+
+export function pieceCollision(x: number, y: number): boolean {
+    // stoppedShapeArray will hold 0 if unoccupied
+    return stoppedShapeArray[y][x] !== 0;
+}
+
+// creates a rotated copy and checks if it fits
+export function rotationCollision(val: number): boolean {
+    // mod: keep index within bounds of array
+    let newRotation = rotationIndex + val;
+    let tetrominoCopy = curTetromino.rotations[mod(newRotation, curTetromino.length)];
+    let collision = false;
+
+    // Cycle through all Tetromino square blocks
+    for (let i = 0; i < tetrominoCopy.length; i++) {
+        let square = tetrominoCopy[i];
+        let x = square[0] + startX;
+        let y = square[1] + startY;
+        if (wallCollision(x)) {
+            collision = true;
+            break;
+        }
+        if (floorCollision(y)) {
+            collision = true;
+            break;
+        }
+        if (pieceCollision(x, y)) {
+            collision = true;
+            break;
+        }
+    }
+    return collision;
+}
+
+// create a tetromino copy and see if it fits vertically
+export function verticalCollision(val: number): boolean {
+    let tetrominoCopy = curTetromino.rotations[rotationIndex];
+    let collision = false;
+
+    // Cycle through all Tetromino square blocks
+    for (let i = 0; i < tetrominoCopy.length; i++) {
+        let square = tetrominoCopy[i];
+        let x = square[0] + startX;
+        let y = square[1] + startY;
+
+        // moving down: increment y to check for a collison
+        y += val;
+
+        // FloorCollision(x, y+1) for immediate stop...leave off +1 for some "sliding"
+        if (floorCollision(y) || pieceCollision(x, y)) {
+            // always check FloorCollision first, or you may hit array bounds error
+            collision = true;
+            break;
+        }
+    }
+
+    if (collision) {
+        // place tetromino
+        for (let i = 0; i < tetrominoCopy.length; i++) {
+            let square = tetrominoCopy[i];
+            let x = square[0] + startX;
+            let y = square[1] + startY;
+            stoppedShapeArray[y][x] = curTetrominoImage;
+        }
+        checkForCompletedRows();
+        createTetrominoFromNext();
+        drawCurTetrominoAndCheckGameOver();
+        return true;
+    }
+
+    return false;
+}
+
+//create a tetromino copy and see if it fits horizontally
+export function horizontalCollision(val: number): boolean {
+    if (val === 0) { return false; }
+    let tetrominoCopy = curTetromino.rotations[rotationIndex];
+    let collision = false;
+
+    // Cycle through all Tetromino square blocks
+    for (let i = 0; i < tetrominoCopy.length; i++) {
+        let square = tetrominoCopy[i];
+        let x = square[0] + startX;
+        let y = square[1] + startY;
+        x += val;
+
+        if (wallCollision(x)) {
+            collision = true;
+            break;
+        }
+        if (pieceCollision(x, y)) {
+            collision = true;
+            break;
+        }
+    }
+
+    return collision;
+}
