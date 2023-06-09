c6e353df2b2b5abc4e2b53f193efbf6d498f5c90
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-09 15:17:30 -0400

    Install ts-jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '\\.(css|scss|sass|less)$', // Match all CSS file extensions
+  ],
+};
