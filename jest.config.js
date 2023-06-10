e63d059184e1af851f5ac959454b67214446869d
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-10 12:07:14 -0400

    Modify attributes for testing

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
+  testEnvironment: 'jest-environment-jsdom',
   transformIgnorePatterns: [
     '/node_modules/',
     '\\.(css|scss|sass|less)$', // Match all CSS file extensions
